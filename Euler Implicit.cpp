1d58ea5cf2583fee23598acea7a92ccbfdb09f8f
OscarMunoz2001 <118483522+OscarMunoz2001@users.noreply.github.com>
2022-12-03 14:52:18 +0100

    Add files via upload

--- Euler Implicit.cpp
+++ Euler Implicit.cpp
@@ -16,7 +16,7 @@ V=40;
 P_ext=0.5*sigma*V*V;
 t_a=0.025;
 dz=0.02;
-dt=0.5*dz;
+dt=0.005*dz;
 M=t_a/dt + 1.0;
 T_c=(kappa/P_ext)*(36.5+273.15); 
 gamma=dt/(dz*dz);
@@ -24,13 +24,12 @@ gamma=dt/(dz*dz);
 double T[N][M];
 double T_old[N][M];
 double T_new[N];
-double T_guess[M];
-
-T_guess[0]=T_c;
+double T_guess[N];
 
 
 for(i=0; i<N; i++){
 	T_old[i][0]=T_c;
+
 }
 
 for(j=0; j<M; j++){
@@ -39,13 +38,20 @@ for(j=0; j<M; j++){
 }
 
 
+
+for(j=0; j<M; j++){
+	//printf("%d", j);
+}
+
 for(j=1; j<M; j++){
 	
+	
 	for (i = 0; i < N; i++){
-		
 		T_guess[i]=T_old[i][j-1];
 	}
-	for(k=1; k<400; k++){	
+	
+	
+	for(k=1; k<4000; k++){	
 		for(i=1; i<N-1; i++){
 			T_new[i]=(1/(1+2*gamma))*((T_guess[i-1]+T_guess[i+1])*gamma+dt+T_old[i][j-1]);
 			}
@@ -55,18 +61,28 @@ for(j=1; j<M; j++){
 			}
 		}
 		
-	for(i=1; i<N; i++){
+	for(i=1; i<N-1; i++){
 		T_old[i][j]=T_new[i];
 			}
 	}	
 
-for(i=0; i<N; i++){
-	for(j=0; j<N;j++){
-		printf("%lf", T_old[i][j]);
-		}
-		
-}
+	for(j=0; j<M;j++){
+		printf("%lf \t %\lf \n", T_old[55][j], T_old[90][j]);
+	 }
 
+FILE *files[N];
+for (i = 0; i < N; i++)
+{
+    char filename[20];
+    sprintf(filename, "euler_implicit%d.dat", i+1);
+    files[i] = fopen(filename, "w");
+    fprintf(files[i], "Temps \t Temperatura\n");
+    for(j=0; j<M; j++){
+    	fprintf(files[i], "%lf \t %lf\n", j*dt, T_old[i][j]);
+	}
+	fclose(files[i]);
+    
+}
 
 return 0;	
 }
